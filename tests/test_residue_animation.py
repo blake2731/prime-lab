4055992b3eb9a6a70c1312c56731f599578f8970
Blake Schmitt <85618096+blake2731@users.noreply.github.com>
2026-08-14 00:21:33 -0400

    Test current filter highlighting in residue checkpoints

--- tests/test_residue_animation.py
+++ tests/test_residue_animation.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+from prime_lab.filters import filter_candidates
 from ui.residue_animation import build_residue_animation
 
 
@@ -76,3 +77,49 @@ def test_residue_animation_settles_to_exact_prime_5_state():
         23,
         29,
     }
+
+
+def test_residue_checkpoint_keeps_current_filter_eliminations_amber():
+    primes = (
+        2,
+        3,
+        5,
+    )
+
+    figure = build_residue_animation(
+        1,
+        60,
+        primes,
+    )
+
+    _, _, eliminated_by = filter_candidates(
+        1,
+        60,
+        primes,
+    )
+
+    expected_current = int(
+        np.count_nonzero(
+            eliminated_by == 5
+        )
+    )
+
+    final_status = np.asarray(
+        figure.data[0].z
+    )
+
+    final_frame_status = np.asarray(
+        figure.frames[-1].data[0].z
+    )
+
+    assert int(
+        np.count_nonzero(
+            final_status == 2
+        )
+    ) == expected_current
+
+    assert int(
+        np.count_nonzero(
+            final_frame_status == 2
+        )
+    ) == expected_current
